09a3b60bf917caff77f36c1eb6365d3a7c249c66
Nicolas Jouanique <njouanique1996@gmail.com>
2023-06-30 14:33:31 -0400

    Pre-commit run

--- test/test.py
+++ test/test.py
@@ -1,30 +1,41 @@
 import os
 from unittest import TestCase, mock
 import unittest
-
-import src
 from src import main
-class MainTesting(TestCase):
 
+
+class MainTesting(TestCase):
     def setUp(self):
         self.app = main
         main.Main.__init__(main.Main)
-    @mock.patch.dict(os.environ, {"HOST" : "", "TOKEN" : "None","TICKETS" : "0", "T_MAX" : "90", "T_MIN" : "10", "DATABASE" : "DB"})
+
+    @mock.patch.dict(
+        os.environ,
+        {
+            "HOST": "",
+            "TOKEN": "None",
+            "TICKETS": "0",
+            "T_MAX": "90",
+            "T_MIN": "10",
+            "DATABASE": "DB",
+        },
+    )
     def test_env_default_value(self):
         assert os.getenv("HOST") == ""
         assert os.getenv("TOKEN") == "None"
         assert os.getenv("TICKETS") == "0"
         assert os.getenv("T_MAX") == "90"
         assert os.getenv("T_MIN") == "10"
-        assert os.getenv("DATABASE") == 'DB'
+        assert os.getenv("DATABASE") == "DB"
 
     def test_env_set_value(self):
-        assert self.app.Main.HOST == 'GITHUB'
-        assert self.app.Main.TOKEN == 'YES'
+        assert self.app.Main.HOST == "GITHUB"
+        assert self.app.Main.TOKEN == "YES"
         assert self.app.Main.TICKETS == "100"
         assert self.app.Main.T_MAX == "100"
         assert self.app.Main.T_MIN == "100"
-        assert self.app.Main.DATABASE == 'DBENV'
+        assert self.app.Main.DATABASE == "DBENV"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
