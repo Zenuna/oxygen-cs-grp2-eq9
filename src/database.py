94cc3ff3bc12580fcf513878916d566b784d4ba8
Nicolas Jouanique <njouanique1996@gmail.com>
2023-07-25 21:43:31 -0400

    MAJ pour ajout à DB

---
+++ src/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# Linux
+engine = create_engine("sqlite:////tmp/dblabo5.db")
+# Windows
+# engine = create_engine(
+#     "mysql+pymysql://root:mysecretpassword@my-mysql-container:3306/mydatabase"
+# )
+db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    import models
+
+    Base.metadata.create_all(bind=engine)
